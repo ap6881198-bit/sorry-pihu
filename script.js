7ab2a2351ee1880b4243e24f39f837c0ff0f421a
ap6881198-bit <ap6881198@gmail.com>
2026-04-08 14:15:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -16,7 +16,12 @@ function createRain() {
         item.classList.add("fall");
 
         item.style.left = Math.random() * 100 + "vw";
+item.style.top = "-20px";
         item.style.animationDuration = (3 + Math.random() * 5) + "s";
+        // random sideways drift
+item.style.animationTimingFunction = "linear";
+item.style.transform = `translateX(${Math.random() * 100 - 50}px)`;
+        item.style.rotate = (Math.random() * 40 - 20) + "deg";
         item.style.fontSize = (16 + Math.random() * 20) + "px";
 
         container.appendChild(item);
